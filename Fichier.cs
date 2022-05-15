e90c0c94cb660dd4398af5a2f601764ad6ff1867
Tropico <simon.vanmello@std.heh.be>
2022-05-15 23:19:39 +0200

    Add project files.

---
+++ Fichier.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace BlocNote
+{
+    internal class Fichier
+    {
+        private string _chemin = "";
+
+        public string Chemin
+        {
+            get { return _chemin; }
+            set { _chemin = value; }
+        }
+
+        public Fichier()
+        {
+
+        }
+
+        public Fichier(string chemin)
+        {
+            Chemin = chemin;
+        }
+
+        public void Save()
+        {
+            if (Chemin != "")
+            {
+                OpenFileDialog openFileDialog = new();
+
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    Chemin = openFileDialog.FileName;
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
